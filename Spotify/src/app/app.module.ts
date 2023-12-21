c13a1f8680e8a158c654106492d064a951f7a82c
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-12-21 12:05:50 +0100

    create component

--- Spotify/src/app/app.module.ts
+++ Spotify/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LibraryComponent } from './library/library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
